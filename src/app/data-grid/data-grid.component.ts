c081418028977576d928da901cec6b3f92c760f3
Raghav <sragha45@gmail.com>
2018-02-06 17:44:24 +0530

    added values in dummies

--- src/app/data-grid/data-grid.component.ts
+++ src/app/data-grid/data-grid.component.ts
@@ -112,25 +112,25 @@ export class DataGridComponent implements OnInit {
       ip: "192.168.24.37",
       details: [
         {
-          pMemUUID: "pMem UUID goes here",
+          pMemUUID: "074878b:df177178641f:c9948d32:b0c6",
           sizeOfAU: 256,
           freedOrAllocated: 0,
-          consumerList: "consumer goes here",
-          memHandler: "memHandler goes here"
+          consumerList: "64.87.15.78",
+          memHandler: "R"
+        },
+        {
+          pMemUUID: "508c:58d51:a4b21afee21ad:b1d9930:7d7",
+          sizeOfAU: 256,
+          freedOrAllocated: 0,
+          consumerList: "43.16.27.98",
+          memHandler: "WR"
         },
         {
           pMemUUID: "pMem UUID goes here",
           sizeOfAU: 256,
           freedOrAllocated: 0,
-          consumerList: "consumer goes here",
-          memHandler: "memHandler goes here"
-        },
-        {
-          pMemUUID: "pMem UUID goes here",
-          sizeOfAU: 256,
-          freedOrAllocated: 0,
-          consumerList: "consumer goes here",
-          memHandler: "memHandler goes here"
+          consumerList: "14.87.123.247",
+          memHandler: "RW"
         }
       ]
     },
@@ -138,25 +138,25 @@ export class DataGridComponent implements OnInit {
       ip: "198.168.21.44",
       details: [
         {
-          pMemUUID: "pMem UUID goes here",
+          pMemUUID: "x4f1f:80bd1e73c1692:73d7696:51a9f3d",
           sizeOfAU: 256,
           freedOrAllocated: 0,
-          consumerList: "consumer goes here",
-          memHandler: "memHandler goes here"
+          consumerList: "172.21.33.24",
+          memHandler: "W"
         },
         {
-          pMemUUID: "pMem UUID goes here",
+          pMemUUID: "6184:20ca797:1683d0d:e5402429:98b2e3",
           sizeOfAU: 256,
           freedOrAllocated: 0,
-          consumerList: "consumer goes here",
-          memHandler: "memHandler goes here"
+          consumerList: "89.57.12.64",
+          memHandler: "W"
         },
         {
-          pMemUUID: "pMem UUID goes here",
+          pMemUUID: "ce2d4:d91c53ac6:530d851623:9de69804",
           sizeOfAU: 256,
           freedOrAllocated: 0,
-          consumerList: "consumer goes here",
-          memHandler: "memHandler goes here"
+          consumerList: "192.168.78.41",
+          memHandler: "RW"
         }
       ]
     }
@@ -167,22 +167,22 @@ export class DataGridComponent implements OnInit {
       ip: "172.21.33.21",
       details: [
         {
-          pMemUUID: "pMem UUID goes here",
-          providerList: "Providers go here",
-          vmID: "ID of the VM",
-          kernelCache: "Kernel cache UUID"
+          pMemUUID: "618420ca7:971683d:0de540:242998b2e3",
+          providerList: "87.34.16.97",
+          vmID: "dummy",
+          kernelCache: "ce2d4:d91c53ac6:530d851623:9de69804"
         },
         {
-          pMemUUID: "pMem UUID goes here",
-          providerList: "Providers go here",
-          vmID: "ID of the VM",
-          kernelCache: "Kernel cache UUID"
+          pMemUUID: "508c:58d51:a4b21afee21ad:b1d9930:7d7",
+          providerList: "192.168.78.41",
+          vmID: "vm2",
+          kernelCache: "61842:0ca7971683d:0de540242:998b2e3"
         },
         {
-          pMemUUID: "pMem UUID goes here",
-          providerList: "Providers go here",
-          vmID: "ID of the VM",
-          kernelCache: "Kernel cache UUID"
+          pMemUUID: "6184:20ca797:1683d0d:e5402429:98b2e3",
+          providerList: "89.54.10.57",
+          vmID: "vm1",
+          kernelCache: "618420ca:7971683d0:de54024299:8b2e3"
         }
       ]
     },
@@ -190,22 +190,22 @@ export class DataGridComponent implements OnInit {
       ip: "172.21.34.156",
       details: [
         {
-          pMemUUID: "pMem UUID goes here",
-          providerList: "Providers go here",
-          vmID: "ID of the VM",
-          kernelCache: "Kernel cache UUID"
+          pMemUUID: "89c5703:460bea6:e06c649a9aca:98ed8b",
+          providerList: "192.168.48.57",
+          vmID: "vm1",
+          kernelCache: "a23095:8ad154b:67d39eade60:95a1f185"
         },
         {
-          pMemUUID: "pMem UUID goes here",
-          providerList: "Providers go here",
-          vmID: "ID of the VM",
-          kernelCache: "Kernel cache UUID"
+          pMemUUID: "508c:58d51:a4b21afee21ad:b1d9930:7d7",
+          providerList: "10.45.78.87",
+          vmID: "vm2",
+          kernelCache: "c4f1f:80bd1e73c1:69273d7696:51a9f3d"
         },
         {
-          pMemUUID: "pMem UUID goes here",
-          providerList: "Providers go here",
-          vmID: "ID of the VM",
-          kernelCache: "Kernel cache UUID"
+          pMemUUID: "c4f1f:80bd1e73c1692:73d7696:51a9f3d",
+          providerList: "78.84.63.12",
+          vmID: "vm1",
+          kernelCache: "618420:ca7971683:d0de5402429:98b2e3"
         }
       ]
     }
@@ -216,8 +216,8 @@ export class DataGridComponent implements OnInit {
       vmID: "vm1",
       details: [
         {
-          providerIP: "provider IP goes here",
-          regionUUID: "region UUID goes here",
+          providerIP: "198.168.21.44",
+          regionUUID: "7d811e1c:a71cc9a31:06c403d43:61388c",
         }
       ]
     },
@@ -225,8 +225,8 @@ export class DataGridComponent implements OnInit {
       vmID: "vm2",
       details: [
         {
-          providerIP: "provider IP goes here",
-          regionUUID: "region UUID goes here",
+          providerIP: "192.168.24.37",
+          regionUUID: "074878b:df177178641f:c9948d32:b0c6",
         }
       ]
     },
@@ -234,8 +234,8 @@ export class DataGridComponent implements OnInit {
       vmID: "dummy",
       details: [
         {
-          providerIP: "provider IP goes here",
-          regionUUID: "region UUID goes here",
+          providerIP: "198.168.21.44",
+          regionUUID: "4d72f:ca7a2f48:debc5da088f:859ef0:a8",
         }
       ]
     },
@@ -243,8 +243,8 @@ export class DataGridComponent implements OnInit {
       vmID: "CMC",
       details: [
         {
-          providerIP: "provider IP goes here",
-          regionUUID: "region UUID goes here",
+          providerIP: "192.168.24.37",
+          regionUUID: "89c5703:460bea6e0:6c649a9:aca98ed8b",
         }
       ]
     }
